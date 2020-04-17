748187282650c534f830fc17c26fd395f7c08fdc
sampletext32 <sampletext32@bk.ru>
2020-04-17 21:14:16 +0300

    Fix: remove function

--- Ignatkina_2/main.cpp
+++ Ignatkina_2/main.cpp
@@ -2,38 +2,44 @@
 
 using namespace std;
 
-int count_n_before_s(int s, int d)
-{
-	int a = 2;
-	int sum = 0;
-	int n = 0;
-
-	while (sum < s)
-	{
-		sum += a;
-		a += d;
-		n++;
-	}
-	return n;
-}
-
 int main()
 {
 	setlocale(LC_ALL, "russian");
 
 	cout << "Программу сделала Игнаткина Валерия студент группы УТН-111" << "\n";
 	cout << "Программа вычисления сроков обучения Незнайки\n";
-	
+
 	int d = 2;
 	int years = 30;
 	int all_words = 50000;
 
 	for (int i = 100; i <= 1000; i += 100)
 	{
-		cout << i << " слов выучит за " << count_n_before_s(i, d) << " дней\n";
+		int a = 2;
+		int sum = 0;
+		int n = 0;
+
+		while (sum < i)
+		{
+			sum += a;
+			a += d;
+			n++;
+		}
+		cout << i << " слов выучит за " << n << " дней\n";
 	}
 
-	int days_for_all_words = count_n_before_s(all_words, d);
+	int a = 2;
+	int sum = 0;
+	int n = 0;
+
+	while (sum < all_words)
+	{
+		sum += a;
+		a += d;
+		n++;
+	}
+
+	int days_for_all_words = n;
 	cout << all_words << " слов выучит за " << days_for_all_words << " дней\n";
 	double required_years = ceil(days_for_all_words / 365.);
 	cout << "Лет потребуется для изучения " << all_words << " слов: " << required_years << "\n";
